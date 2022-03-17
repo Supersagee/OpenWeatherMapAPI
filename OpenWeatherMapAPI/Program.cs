a49d5095f76b78c5ae57f6ba3f0e11975be354dc
Supersagee <williamdsage@gmail.com>
2022-03-16 23:13:35 -0500

    try-catch added

--- OpenWeatherMapAPI/Program.cs
+++ OpenWeatherMapAPI/Program.cs
@@ -12,15 +12,42 @@ namespace OpenWeatherMapAPI
 
             var weather = new Weather(client);
 
+            var conditional = true;
+
             while (true)
             {
-                Console.WriteLine("What city would you like to know the weather in?");
-                weather.UserCity = Console.ReadLine();
-                weather.WeatherURL = $"https://api.openweathermap.org/data/2.5/find?q={weather.UserCity}&units=imperial&appid=c6a5411956e8b651b9d50e35410b9b06";
+                while (conditional)
+                {
+                    try
+                    {
+                        Console.WriteLine("In which city would you like to check the weather?");
+                        weather.UserCity = Console.ReadLine();
+                        weather.WeatherURL = $"https://api.openweathermap.org/data/2.5/find?q={weather.UserCity}&units=imperial&appid=c6a5411956e8b651b9d50e35410b9b06";
 
-                Console.WriteLine($"It is currently {weather.CurrentTemp()} degrees in {weather.UserCity} with {weather.CurrentConditions()}.");
-                Console.WriteLine($"You can expect a high of {weather.HighTemp()}, and a low of {weather.LowTemp()} today.");
-                Console.WriteLine($"-----------------------------------------------------------------------");
+                        Console.WriteLine($"It is currently {weather.CurrentTemp()} degrees in {weather.UserCity} with {weather.CurrentConditions()}.");
+                        Console.WriteLine($"You can expect a high of {weather.HighTemp()}, and a low of {weather.LowTemp()} today.");
+                        Console.WriteLine($"-----------------------------------------------------------------------");
+                        conditional = false;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("-----------------------------------------------------------------");
+                        Console.WriteLine("Unable to get the weather for that city. Please try another city.");
+                        Console.WriteLine("-----------------------------------------------------------------");
+                    }
+                }
+
+                conditional = true;
+
+                Console.WriteLine("Would you like to check the weather in another city? yes or no:");
+                var yesOrNo = Console.ReadLine();
+                
+                if (yesOrNo.ToLower() == "no")
+                {
+                    break;
+                }
+
+                Console.Clear();
             }
 
         }
